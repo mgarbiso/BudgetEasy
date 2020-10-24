e297e93a9e000a840158cf6231b9c4210249b0c8
mgarbiso <markus.a.garbiso@gmail.com>
2020-10-23 23:31:44 -0500

    Added error msg if one tries to add and subtract moneies of different units.

--- src/Money.jl
+++ src/Money.jl
@@ -7,13 +7,25 @@ struct Money
     units::String
 end
 
-#To Do, for adding and subtracting money you have to compare the currencies.
+#Function to check the units
+"""This checkunits checks the units of to Money objects to see if they have
+   the same units."""
+function checkunits(money1::Money,money2::Money)
+    money1.units == money2.units ? true : error("The units need to match between $(money1::Money) and $(money2::Money).")
+end
 
 #You can add money to money.
-+(money1::Money,money2::Money) = Money(money1.value + money2.value, money1.units)
+function +(money1::Money,money2::Money)::Money
+    checkunits(money1,money2)
+    Money(money1.value + money2.value, money1.units)
+end
 
 #You subtract add money to money.
--(money1::Money,money2::Money) = Money(money1.value - money2.value, money1.units)
+function -(money1::Money,money2::Money)::Money
+    checkunits(money1,money2)
+    Money(money1.value - money2.value, money1.units)
+end
+
 
 #You can multiply money by numbers.
 *(money1::Money,number::Real) = Money(money1.value*number, money1.units)
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ using BudgetEasy, Test
 
 m1 = BudgetEasy.Money(4, "monies")
 m2 = BudgetEasy.Money(10, "monies")
+m3 = BudgetEasy.Money(10, "dollars")
 
 num1 = 6
 num2 = 8
@@ -13,3 +14,6 @@ num2 = 8
 @test m1 - m2 == BudgetEasy.Money(-6, "monies")
 @test m1/num1 == BudgetEasy.Money(4/6, "monies")
 # @test num2\m2 == BudgetEasy.Money(8\10, "monies")
+
+@test_throws ErrorException m3 + m1
+@test_throws ErrorException m3 - m1
