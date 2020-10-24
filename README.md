64e3cf92388148d69ee6c2f9cc974767bba5498b
mgarbiso <markus.a.garbiso@gmail.com>
2020-10-23 20:24:43 -0500

    first commit of julia files.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # BudgetEasy
-This is a simple budgeting library for julia that is my personal project.
+This is a simple budgeting library for Julia that is my personal project.
---
+++ src/BudgetEasy.jl
@@ -0,0 +1,28 @@
+module BudgetSimple
+
+const name = "BudgetSimple"
+
+using JSON3, Currencies
+
+# File Modification Section
+
+# This creates a buget (JSON) file.
+
+#Import Raw data!
+
+# Add an entries to files.
+
+
+#Objects
+# Entry object that should be Abstract different types of entries would be
+# expenses, incomes
+
+# Use Currences.jl for expressing amounts of money.
+
+
+# Create Pretty various plots, maybe Gadfly or plots.
+# Over a period of time!
+#
+
+
+end # module
